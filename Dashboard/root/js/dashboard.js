4ff0527a016bfcabf98dbbb2df04077a1745b389
SHB742 <shoaib.omar.17@ucl.ac.uk>
2018-12-29 17:23:34 +0530

    updated dashboard

--- Dashboard/root/js/dashboard.js
+++ Dashboard/root/js/dashboard.js
@@ -1,9 +1,7 @@
 var tableRef = document.getElementById('Alerts').getElementsByTagName('tbody')[0];
 var table;
 var latestAlert;
-const alertsPerPage = 10;
-var totalAlerts = 0;
-var newAlerts = 0;
+const maxAlerts = 100;
 
 function playAudio(elem){ 
     if (elem.audio != undefined) {
@@ -42,57 +40,67 @@ function playAudio(elem){
 }
 
 
-function updateAlerts(page_num,update){
-    if ((page_num * alertsPerPage)<totalAlerts){
-        var jqxhr = $.getJSON( "/api/alerts/?file=none&limit="+alertsPerPage+"&skip="+(page_num*alertsPerPage))
+function updateAlerts(first_update){
+   // if ((page_num * alertsPerPage)<totalAlerts){ //+"&skip="+(page_num*alertsPerPage))
+        var jqxhr = $.getJSON( "/api/alerts/?file=none&limit="+maxAlerts)
           .done(function(json) {
-            for (newAlerts;newAlerts>0;newAlerts--){
-                var item = json.pop();
-                table.row.add([item["_id"],item["content"],item["location"],new Date(item["createdAt"]).toString(),"<a style='font-size: 100%;cursor: pointer;' idd='"+item["_id"]+"' class='fa fa-play' onclick='playAudio(this)'></a>"]).draw();
-                if(update == false){
-                    tableRef.rows[0].classList.remove("highlight");//don't have highlight animation on first load
-                }
+            var latest = tableRef.rows[0].cells[0].innerText;
+            while ((json.length > 0) && (first_update || (json[0]["_id"] != latest))) {
+                var item = json.shift();
+                table.row.add([item["_id"],item["content"],item["location"],new Date(item["createdAt"]).toString(),"<a style='font-size: 100%;cursor: pointer;' first='"+first_update+"' idd='"+item["_id"]+"' class='fa fa-play' onclick='playAudio(this)'></a>"]);
             }
+            table.draw();
           })
           .fail(function(err) {
             alert( "error" );
             window.location.replace("/pages/login.html");
         });
-    }
+    //}
     
 }
 
 //change this will not work when operating at alert limit (change to get newest alert and compare ids)
-function checkForAlerts(update){
-    var jqxhr = $.getJSON( "/api/alerts/count/")
-      .done(function(json) {
-        newAlerts = json["TotalAlerts"] - totalAlerts;
-        totalAlerts = json["TotalAlerts"];
-        updateAlerts(0,update)
-      })
-      .fail(function() {
-        alert( "error" );
-        window.location.replace("/pages/login.html");
-      });
+function checkForAlerts(first_update){
+    if (first_update) {
+        //set up alerts table
+        table = $('#Alerts').DataTable({
+            "createdRow": function(row, data, dataIndex) {
+                if ( $(row.cells[4].children[0]).attr("first") == "false" ){
+                    $(row).attr('class',"highlight");
+                }
+                $(row).attr('style',"text-align: center;vertical-align: middle;");
+                $(row).attr('onclick','(function(elem){ elem.classList.remove("highlight"); })(this);');
+            },
+            responsive: true,
+            autoWidth: true,
+            order: [[ 3, "desc" ]],
+            columnDefs: [ {
+                "targets": [0,1,2,3,4],
+                "orderable": false
+                } ]
+        });
+        //alertsPerPage = table.page.info()["length"];
+        updateAlerts(first_update);
+    }else{
+        var newXhr = $.getJSON( "/api/alerts/?file=none&limit=1")
+        .done(function(json) {
+            if (json[0]["_id"] != tableRef.rows[0].cells[0].innerText) {
+                updateAlerts(first_update);
+            }
+
+        })
+        .fail(function() {
+                alert( "error" );
+                window.location.replace("/pages/login.html");
+        });
+    }
 }
 
-
  $(document).ready(function() {
-    //set up alerts table
-    table = $('#Alerts').DataTable({
-        "createdRow": function(row, data, dataIndex) {
-        $(row).attr('style',"text-align: center;vertical-align: middle;");
-        $(row).attr('class',"highlight");
-        $(row).attr('onclick','(function(elem){ elem.classList.remove("highlight"); })(this);');
-        },
-        responsive: true,
-        order: [[ 3, "desc" ]]
-    });
-    checkForAlerts(false);
-    setInterval(function(){checkForAlerts(true); }, 30000);//check for new alerts every half-minute
+    checkForAlerts(true);
+    setInterval(function(){checkForAlerts(false); }, 30000);//check for new alerts every half-minute 30000
     
 });
-
 
 
 
