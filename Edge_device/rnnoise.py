2f463b62ef5f2fdb96a10af74ff52bb4ef63934b
SHB742 <shoaib.omar.17@ucl.ac.uk>
2019-01-23 21:27:55 +0000

    minor updates

--- Edge_device/rnnoise.py
+++ Edge_device/rnnoise.py
@@ -3,12 +3,13 @@ from ctypes import util
 
 lib_path = util.find_library("rnnoise")
 if (not("/" in lib_path)):
-	lib_path = os.popen('ldconfig -p | grep '+lib_path).read().split('\n')[0].split(" ")[-1]
+	lib_path = os.popen('ldconfig -p | grep '+lib_path).read().split('\n')[0].strip().split(" ")[-1]
 	
 lib = ctypes.cdll.LoadLibrary(lib_path)
 lib.rnnoise_process_frame.argtypes = [ctypes.c_void_p,ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_float)]
 lib.rnnoise_process_frame.restype = ctypes.c_float
 lib.rnnoise_create.restype = ctypes.c_void_p
+lib.rnnoise_destroy.argtypes = [ctypes.c_void_p]
 
 class RNNoise(object):
 	def __init__(self):
