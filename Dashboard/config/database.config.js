67901d9c12f4b0593e55466d4c4788ae4376d63c
Shb742 <32335388+Shb742@users.noreply.github.com>
2019-01-28 00:09:41 +0000

    Update database.config.js

--- Dashboard/config/database.config.js
+++ Dashboard/config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: process.env.MONGODB_URI || "mongodb+srv://test:2m8mq4gAf1qvNwHF@edvscluster-b0auk.mongodb.net/test?retryWrites=true"
+	url : process.env.MONGODB_URI || 'mongodb://localhost:27017/dashboard'
 }
