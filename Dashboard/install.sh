9737fd110a530d7d592c7916b9d61b56abb3a126
dragonxi <dragonxi@users.noreply.github.com>
2019-04-09 13:36:02 +0100

    Update install.sh

--- Dashboard/install.sh
+++ Dashboard/install.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 # Check if running in a supported Linux distro.
 if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
     echo "Sorry, EDVS Dashboard does not support WSL(Windows Subsystem for Linux) yet."
-	echo "For more details on supported platforms, see project README."
-	exit 1
+    echo "For more details on supported platforms, see project README."
+    exit 1
 fi
 if [ "$(uname)" ">" "MINGW" -a "$(uname)" "<" "MINGX" ] ; then
     echo "Sorry, EDVS Dashboard does not support MinGW(Minimalist GNU for Windows) yet."
@@ -26,10 +26,16 @@ if [ "$(uname)" != "Linux" ] ; then
 fi
 
 # If ran with -all argument, then install Node.js and MongoDB first.
-if "$1" = "-all" ; then
-./install-nodejs.sh
-./install-mongodb.sh
-fi
+case $1 in
+    -a | --all)
+        ./install-nodejs.sh
+        ./install-mongodb.sh
+        ;;
+    *)
+        echo "Invalid argument."
+        exit 1
+        ;;
+esac
 
 echo ""
 echo "##############################"
@@ -42,71 +48,71 @@ echo ""
 # Check for supported Node.js
 if which node > /dev/null ; then
     echo "Node.js is found."
-	if /usr/bin/env node -v | grep -E "(v8|v10)" &> /dev/null ; then
-	    /usr/bin/env node -v
-		echo ""
-		else
-		    echo "Current Node.js version is not compatible with EDVS Dashboard."
-			echo "You can install Node.js by running:"
-			echo ""
-			echo "    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
-			echo "    sudo apt-get install -y nodejs"
-			echo ""
-			echo "or by running \"./install-nodejs.sh\"."
-			echo "or by re-running this script with \"-all\" argument to install them first."
-			echo ""
-			echo "Please install a newer version of Node.js and re-run this script."
-			echo "The installer will now exit."
-			exit 1
-	fi
-	else
-		echo "Node.js is not found."
-		echo "You can install Node.js by running:"
-		echo ""
-		echo "    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
-		echo "    sudo apt-get install -y nodejs"
+    if /usr/bin/env node -v | grep -E "(v8|v10)" &> /dev/null ; then
+        /usr/bin/env node -v
         echo ""
-		echo "or by running \"./install-nodejs.sh\"."
-		echo "or by re-running this script with \"-all\" argument to install them first."
-		echo ""
-		echo "Please install a compatible Node.js and re-run this script."
-		echo "The installer will now exit."
-		exit 1
+        else
+            echo "Current Node.js version is not compatible with EDVS Dashboard."
+            echo "You can install Node.js by running:"
+            echo ""
+            echo "    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
+            echo "    sudo apt-get install -y nodejs"
+            echo ""
+            echo "or by running \"./install-nodejs.sh\"."
+            echo "or by re-running this script with \"-all\" argument to install them first."
+            echo ""
+            echo "Please install a newer version of Node.js and re-run this script."
+            echo "The installer will now exit."
+            exit 1
+    fi
+    else
+        echo "Node.js is not found."
+        echo "You can install Node.js by running:"
+        echo ""
+        echo "    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
+        echo "    sudo apt-get install -y nodejs"
+        echo ""
+        echo "or by running \"./install-nodejs.sh\"."
+        echo "or by re-running this script with \"-all\" argument to install them first."
+        echo ""
+        echo "Please install a compatible Node.js and re-run this script."
+        echo "The installer will now exit."
+        exit 1
 fi
 
 # Check for supported MongoDB
 MONGODB_URI=""
 while true; do
     read -p "Will the MongoDB database be on this server too (y/n)? " -n 1 -r
-	echo ""
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]] ; then
-		if systemctl is-active mongod | grep -qE "(inactive)" &> /dev/null ; then
-			echo "A running MongoDB is not found on this server,"
-			echo "Either it is not installed, or it is installed but mongod daemon is not started."
-			echo "You can install MongoDB by running:"
-			echo ""
-			echo "    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4"
-			echo "    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list"
-			echo "    sudo apt-get update"
-			echo "    sudo apt-get install -y mongodb-org"
-			echo "    sudo service mongod start"
-			echo ""
-			echo "or by running \"./install-mongodb.sh\"."
-			echo "or by re-running this script with \"-all\" argument to install them first."
-			echo "Please re-run this script after installation."
-			echo "The installer will now exit."
-			exit 1
-			else
-			    MONGODB_URI="mongodb://localhost:27017/dashboard"
-		fi
-		break
-	fi
-	if [[ $REPLY =~ ^[Nn]$ ]] ; then
-	    read -p "In that case, please enter an URI for the MongoDB server: " MONGODB_URI
-		break
-	fi
-	echo "Invalid input, please enter again."
+    echo ""
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]] ; then
+        if systemctl is-active mongod | grep -qE "(inactive)" &> /dev/null ; then
+            echo "A running MongoDB is not found on this server,"
+            echo "Either it is not installed, or it is installed but mongod daemon is not started."
+            echo "You can install MongoDB by running:"
+            echo ""
+            echo "    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4"
+            echo "    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list"
+            echo "    sudo apt-get update"
+            echo "    sudo apt-get install -y mongodb-org"
+            echo "    sudo service mongod start"
+            echo ""
+            echo "or by running \"./install-mongodb.sh\"."
+            echo "or by re-running this script with \"-all\" argument to install them first."
+            echo "Please re-run this script after installation."
+            echo "The installer will now exit."
+            exit 1
+            else
+                MONGODB_URI="mongodb://localhost:27017/dashboard"
+        fi
+        break
+    fi
+    if [[ $REPLY =~ ^[Nn]$ ]] ; then
+        read -p "In that case, please enter an URI for the MongoDB server: " MONGODB_URI
+        break
+    fi
+    echo "Invalid input, please enter again."
 done
 
 # Modify .env file.
